5482cc9b76087c84bdee0727969ec9f1a132c4fd
cho45 <cho45@lowreal.net>
2008-07-06 02:49:41 +0000

    チャンネルメッセージになってなかった……

--- examples/wig.rb
+++ examples/wig.rb
@@ -521,7 +521,7 @@ class WassrIrcGateway < Net::IRC::Server::Session
 									log "#{nick}: #{body}"
 								when body =~ /^#([a-z_]+)\s+(.+)$/i
 									# channel message or not
-									message(id || nick, Regexp.last_match[1], Regexp.last_match[2])
+									message(id || nick, "##{Regexp.last_match[1]}", Regexp.last_match[2])
 								else
 									message(id || nick, main_channel, body)
 								end
