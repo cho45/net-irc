2578b4d1993b3b74b12d6a7a93250a02eea3cd13
cho45 <cho45@lowreal.net>
2008-08-22 09:31:23 +0000

    変なときに reply のやつが走ってきもちわるいのでスペース必須に

--- examples/wig.rb
+++ examples/wig.rb
@@ -257,7 +257,7 @@ class WassrIrcGateway < Net::IRC::Server::Session
 		begin
 			if target =~ /^#(.+)/
 				channel = Regexp.last_match[1]
-				reply   = message[/>(.+)$/, 1]
+				reply   = message[/\s+>(.+)$/, 1]
 				if !reply && @opts.key?("alwaysim") && @im && @im.connected? # in jabber mode, using jabber post
 					message = "##{channel} #{message}" unless "##{channel}" == main_channel
 					ret = @im.deliver(jabber_bot_id, message)
