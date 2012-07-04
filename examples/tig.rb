d82bc8702857afbc70f6074de7b4ede914adb88f
cho45 <cho45@lowreal.net>
2012-07-04 17:39:35 +0900

    expand t.co
    メッセージ自体に展開URLを含んでるんだけど、めんどうなのでここでやってる。そのうちなおしたい

--- examples/tig.rb
+++ examples/tig.rb
@@ -2087,7 +2087,7 @@ class TwitterIrcGateway < Net::IRC::Server::Session
 				(?: bit\.ly | (?: tin | rub) yurl\.com | j\.mp
 				  | is\.gd | cli\.gs | tr\.im | u\.nu | airme\.us
 				  | ff\.im | twurl.nl | bkite\.com | tumblr\.com
-				  | pic\.gd | sn\.im | digg\.com )
+				  | pic\.gd | sn\.im | digg\.com | t\.co)
 				/ [0-9a-z=-]+ |
 				blip\.fm/~ (?> [0-9a-z]+) (?! /) |
 				flic\.kr/[a-z0-9/]+
